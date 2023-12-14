ea63c751ca13dc2a072308bda2813ecf2deb7a88
TatianaMartynova <2gabbrie@gmail.com>
2023-12-14 19:38:05 +0300

    keysolo done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,15 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+        document.addEventListener('keydown', (event) => {
+          const enteredKey = event.key.toLowerCase();
+      
+          if (enteredKey === this.currentSymbol.textContent.toLowerCase()) {
+            this.success();
+          } else {
+            this.fail();
+          }
+        });
   }
 
   success() {
