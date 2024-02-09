719b1a112df308d677f5dbf6423e4cdd3fce4120
TatianaMartynova <2gabbrie@gmail.com>
2024-02-09 19:38:17 +0300

    progressbar_done

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,36 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const progress = document.getElementById('progress');
-    const form = document.getElementById('form');
-    const fileInput = document.getElementById('file');
-    const sendButton = document.getElementById('send');
-  
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-  
-      const formData = new FormData(form);
-  
-      const xhr = new XMLHttpRequest();
-  
-      xhr.upload.addEventListener('progress', function (event) {
-        if (event.lengthComputable) {
+  const progress = document.getElementById('progress');
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', async function (event) {
+      event.preventDefault();
+
+      try {
+          const formData = new FormData(form);
+          const xhr = new XMLHttpRequest();
+
+          xhr.upload.addEventListener('progress', updateProgress);
+
+          xhr.addEventListener('load', handleLoad);
+          xhr.addEventListener('error', handleError);
+          xhr.addEventListener('abort', handleAbort);
+
+          xhr.open('POST', form.action, true);
+          xhr.send(formData);
+      } catch (error) {
+          console.error('Error during form submission:', error);
+      }
+  });
+
+  function updateProgress(event) {
+      if (event.lengthComputable) {
           const percentComplete = (event.loaded / event.total) * 100;
           progress.value = percentComplete;
-        }
-      });
-  
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          
-          if (xhr.status === 200) {
-            alert('Файл успешно загружен!');
-          } else {
-            alert('Ошибка загрузки файла. Пожалуйста, попробуйте еще раз.');
-          }
-        }
-      };
-  
-      xhr.open('POST', form.action, true);
-      xhr.send(formData);
-    });
-  });
-  
+      }
+  }
+
+  function handleLoad() {
+      if (xhr.status === 200) {
+          alert('Файл успешно загружен!');
+      } else {
+          alert('Произошла ошибка при загрузке файла. Пожалуйста, повторите попытку.');
+      }
+  }
+
+  function handleError() {
+      alert('Произошла ошибка при загрузке файла. Пожалуйста, повторите попытку.');
+  }
+
+  function handleAbort() {
+      alert('Загрузка файла была отменена.');
+  }
+});
